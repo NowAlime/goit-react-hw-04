71cff68c9ffc4f7885251ad4807552388111c17a
NowAlime <147753857+NowAlime@users.noreply.github.com>
2024-05-21 14:52:29 +0300

    add

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,97 +6,78 @@ import fetchImages from "../fetchImg";
 import ImageModal from "../ImageModal/ImageModal";
 import ImageGallery from "../ImageGallery/ImageGallery";
 import Loader from '../Loader/Loader';
-import "./App.module.css";
 
- function App (){
-    const [error, setError] = useState(null);
-    const [isOpen, setIsOpen] = useState(false);
-    const [loading, setLoading] = useState(false);
-    const [images, setImages] = useState([]);
-    const [page, setPage] = useState(1);
-    const [searchTerm, setSearchTerm] = useState("");
-   const [selectedImages, setSelectedImages] = useState("");
-    const [endOfCollection, setEndOfCollection] = useState(false);
-    const [hasLoadedImages, setHasLoadedImages] = useState(false);
-    const [totalCollection, setTotalCollection] = useState({});
-    const [totalImages, setTotalImages] = useState(0);
 
-    const handleOpenModal = (imageUrl) => {
-        setSelectedImages(imageUrl);
-        setIsOpen(true);
-      };
-      const handleCloseModal = () => {
-        setSelectedImages("");
-        setIsOpen(false);
-      };
+function App() {
+  const [error, setError] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedImage, setSelectedImage] = useState("");
+  const [totalPages, setTotalPages] = useState(0);
 
-      const handleSearch = async (searchTerm) => {
-        try {
-          setImages([]);
-          setLoading(true);
-          setError(false);
-          setPage(1);
-          setSearchTerm(searchTerm);
-          const data = await fetchImages(searchTerm);
-          setImages(data.results);
-          setTotalCollection(data.total);
-          setEndOfCollection(false);
-          setHasLoadedImages(true);
-        } catch (error) {
-          setError(true);
-        } finally {
-          setLoading(false);
-        }
-      };
-      useEffect(() => {
-        const fetchTotalImages = async () => {
-          const total = Math.ceil(totalCollection / 15);
-          setTotalImages(total);
-        };
-    
-        fetchTotalImages();
-      }, [totalCollection]);
-      useEffect(() => {
-        if (page >= totalImages) {
-          setEndOfCollection(true);
-        } else {
-          setEndOfCollection(false);
-        }
-      }, [page, totalImages]);
-      const handleLoadMore = async () => {
-        try {
-          setLoading(true);
-          const nextPageData = await fetchImages(searchTerm, page + 1);
-          setPage((prevPage) => prevPage + 1);
-          setImages((prevImages) => [...prevImages, ...nextPageData.results]);
-        } catch (error) {
-          setError(true);
-        } finally {
-          setLoading(false);
-        }
-      };
-      return  (
-        <>
-          <SearchBar onSearch={handleSearch} />
-          {images.length > 0 && (
-            <ImageGallery images={images} isOpen={handleOpenModal} />
-          )}
-         {endOfCollection && hasLoadedImages && 'The end'}
-          {error && <ErrorMessage searchTerm={searchTerm} setError={setError} />}
-          {loading && <Loader />}
-          {images.length > 0 && !endOfCollection && (
-            <LoadMoreBtn onChange={handleLoadMore} />
-          )}
-          <ImageModal
-            images={images}
-            isOpen={isOpen}
-            isClose={handleCloseModal}
-            imageUrl={selectedImages}
-          />
-        </>
-      );
-    
-    
-     }
-    
-     export default App;
+  const handleOpenModal = (imageUrl) => {
+    setSelectedImage(imageUrl);
+    setIsOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedImage("");
+    setIsOpen(false);
+  };
+
+  const handleSearch = (searchTerm) => {
+    setImages([]);
+    setPage(1);
+    setSearchTerm(searchTerm);
+  };
+
+  useEffect(() => {
+    if (!searchTerm) return;
+
+    const fetchImagesData = async () => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const data = await fetchImages(searchTerm, page);
+        setImages((prevImages) => (page === 1 ? data.results : [...prevImages, ...data.results]));
+        setTotalPages(Math.ceil(data.total / 15));
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchImagesData();
+  }, [searchTerm, page]);
+
+  const handleLoadMore = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  return (
+    <>
+      <SearchBar onSearch={handleSearch} />
+      {images.length > 0 && (
+        <ImageGallery images={images} isOpen={handleOpenModal} />
+      )}
+      {error && <ErrorMessage searchTerm={searchTerm} setError={setError} />}
+      {loading && <Loader />}
+      {page < totalPages && !loading && (
+        <LoadMoreBtn onChange={handleLoadMore} />
+      )}
+      <ImageModal
+        images={images}
+        isOpen={isOpen}
+        isClose={handleCloseModal}
+        imageUrl={selectedImage}
+      />
+    </>
+  );
+}
+
+export default App;
